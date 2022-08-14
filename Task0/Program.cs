2c9dbcc09f58b74b06f980ab9395a5b1d4270c2d
Svetlana-18 <vasilevichsvetlana@yandex.ru>
2022-08-14 17:44:41 +0300

    проверила решения, подготовила к отправке в GitHub

---
+++ Task0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1   
+
+Console.WriteLine("Введите трехзначное число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int NumberSearch(int num)
+    {
+        int secondten = num % 100;
+        int secondtdigit = (secondten - num % 10) / 10;
+        return secondtdigit;
+    }
+    int result = NumberSearch(number);
+    Console.WriteLine($"вторая цифра числа {number} -> {result}");
+}
+else Console.WriteLine("вы ввели не трехзначное число, повторите ввод");
---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6        
+
+
+Console.WriteLine("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberSearch(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    int thirddigit = num % 10;
+
+    return thirddigit;
+}
+int result = NumberSearch(number);
+if (number > 99)
+{
+    Console.WriteLine($"{number} -> {NumberSearch(number)}");
+}
+else
+{
+    Console.WriteLine("-> третьей цифры нет");
+
+}
+
+
+
